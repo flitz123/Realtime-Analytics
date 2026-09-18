ab2b9861e5b878d7c1b55fdb32fcf0fb55991a5b
flitz123 <127119426+flitz123@users.noreply.github.com>
2026-09-18 13:58:56 +0400

    new

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    
-    "github.com/gorilla/mux"
-    
-    "github.com/flitz123/RealTime-Analytics/internal/analytics"
-    "github.com/flitz123/RealTime-Analytics/internal/handler"
-    "github.com/flitz123/RealTime-Analytics/internal/store"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/flitz123/RealTime-Analytics/internal/analytics"
+	"github.com/flitz123/RealTime-Analytics/internal/handler"
+	"github.com/flitz123/RealTime-Analytics/internal/store"
 )
 
-func main(){
+func main() {
 	eventStore := store.NewEventStore()
-	processor := analytics.NewProcessor(eventStore) 
+	processor := analytics.NewProcessor(eventStore)
 	processor.Start()
 
 	wsHub := handler.NewWebSocketHub(eventStore, processor)
@@ -28,7 +28,7 @@ func main(){
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./web")))
 
 	log.Println("Server starting on: 8080")
-	if err := http.ListenAndServe(":8080", router); err != nil{
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal("Server failed to start:", err)
 	}
 }
